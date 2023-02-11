73424ab7068526a808f394e5e62b230767ab3987
AaKuGu <prathamtogupta11@gmail.com>
2023-02-12 03:00:31 +0530

    re

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='relative z-20'>Send Email ( NodeMailer )</div>
+    <div className='relative z-20 text-xl md:text-3xl'>Send Email ( NodeMailer )</div>
   )
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,8 @@ import toast, { Toaster } from "react-hot-toast";
 
 const Home = () => {
   const [email, setEmail] = useState("");
-  const inputRef = useRef()
+  const [ress, setRess] = useState();
+  const inputRef = useRef();
 
   const sendEmail = async (e) => {
     e.preventDefault();
@@ -12,8 +13,10 @@ const Home = () => {
     const res = await axios.post("http://localhost:8000/api/register", {
       email,
     });
+
+    setRess("Hii");
     toast("Email sent, Check your inbox ...!");
-    console.log(res);
+
   };
 
   useEffect(() => {
@@ -21,7 +24,7 @@ const Home = () => {
   });
   return (
     <div
-      className="text-xl gap-16 flex flex-col px-5 py-5 rounded-lg w-[80%] md:w-[50%] z-20"
+      className="text-xl gap-16 flex flex-col px-5 py-5 rounded-lg w-[80%] md:w-[70%] xl:w-[70%] z-20"
       style={{ background: "blue" }}
     >
       <Toaster position="top-center" reverseOrder="false" />
@@ -33,14 +36,16 @@ const Home = () => {
         onSubmit={sendEmail}
         className="flex flex-col items-center gap-5"
       >
-        <div className="flex justify-center gap-5 md:text-3xl text-xl flex-col md:flex-row items-center">
-          <label htmlFor="email" className="font-mono">Email : </label>
+        <div className="flex justify-center md:gap-0 gap-5 md:text-3xl text-xl flex-col md:flex-row items-center">
+          <label htmlFor="email" className="font-mono">
+            Email :{" "}
+          </label>
           <input
             type="email"
             name="email"
             id="email"
             onChange={(e) => setEmail(e.target.value)}
-            className="border-none outline-none px-5 font-bold text-lg min-w-[200px] max-w-[300px] rounded-lg bg-transparent"
+            className="border-none outline-none px-5 font-bold text-base md:text-2xl w-[200px]  rounded-lg bg-gray-700 md:bg-transparent mb-10 md:mb-0 py-2"
             style={{ color: "white" }}
             ref={inputRef}
           />
@@ -48,13 +53,14 @@ const Home = () => {
 
         <button
           type="submit"
-          className="px-5 py-2 hover:bg-red-900 rounded-lg w-[130px] text-2xl"
+          className="md:px-5 md:py-2 hover:bg-red-900 rounded-lg md:text-2xl text-lg px-3 py-1"
           style={{ background: "purple" }}
           onClick={() => toast("Sending...")}
         >
           Receive
         </button>
       </form>
+      <div>{ress}</div>
     </div>
   );
 };
